d41cba1b04fbd0d397c2aa8191bbf79c15ed97cb
Xnanga <jpeutherer1@hotmail.co.uk>
2023-03-22 23:41:43 +0000

    Fix rendering of nested emotion accordion buttons and add proptypes

--- client/components/buttons/AccordionButton.js
+++ client/components/buttons/AccordionButton.js
@@ -1,12 +1,34 @@
+import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import { useState } from 'react';
+import AddPlusCircle from '../svg-components/AddPlusCircle';
+
+const BarButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const BarButton = styled.button`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   height: 5rem;
+  padding: 1rem;
+  text-align: center;
+  text-transform: capitalize;
   background-color: ${({ colorHex }) => colorHex || '#ffffff'};
-  box-shadow: 2px 2px 0px 0px rgba(0, 0, 0, 0.15);
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
   cursor: pointer;
+
+  &:hover {
+    filter: brightness(95%);
+  }
+
   & > span {
+    margin: 0 auto;
     background: inherit;
     background-clip: text;
     color: transparent;
@@ -17,43 +39,60 @@ const BarButton = styled.button`
   }
 `;
 
+const BarButtonIconContainer = styled.div`
+  position: absolute;
+  top: 21%;
+  right: 1rem;
+  filter: invert(1) grayscale(1) contrast(100);
+  pointer-events: none;
+`;
+
+const HiddenContentContainer = styled.div`
+  padding: 1.25rem;
+  background-color: lightgray;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+`;
+
 const AccordionButton = ({
   label,
+  iconSrc,
+  onClick,
   colorHex,
-  level,
-  onSelectEmotion,
-  hiddenContent,
-  currentlySelected,
+  hiddenJSXContent,
 }) => {
   const [accordionOpen, setAccordionOpen] = useState(false);
 
   const handleClick = (e) => {
-    if (hiddenContent) setAccordionOpen((prevState) => !prevState);
-    onSelectEmotion(e.target.textContent);
+    setAccordionOpen((prevState) => !prevState);
+    onClick(e.target.textContent);
   };
 
   return (
-    <>
-      <BarButton colorHex={colorHex} onClick={handleClick} level={level}>
+    <BarButtonContainer>
+      <BarButton colorHex={colorHex} onClick={(e) => handleClick(e)}>
         <span>{label}</span>
+        {iconSrc && (
+          <BarButtonIconContainer>
+            <AddPlusCircle colorHex={colorHex} />
+          </BarButtonIconContainer>
+        )}
       </BarButton>
-      {accordionOpen &&
-        hiddenContent &&
-        hiddenContent.map((emotion) => {
-          return (
-            <AccordionButton
-              key={Math.random()}
-              label={emotion.name}
-              colorHex={emotion.colorHex}
-              onSelectEmotion={onSelectEmotion}
-              level={emotion.level}
-              hiddenContent={emotion?.children}
-              currentlySelected={currentlySelected}
-            />
-          );
-        })}
-    </>
+      {accordionOpen && hiddenJSXContent && (
+        <HiddenContentContainer colorHex={colorHex}>
+          {hiddenJSXContent}
+        </HiddenContentContainer>
+      )}
+    </BarButtonContainer>
   );
 };
 
 export default AccordionButton;
+
+AccordionButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  iconSrc: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+  colorHex: PropTypes.string,
+  hiddenJSXContent: PropTypes.element,
+};
